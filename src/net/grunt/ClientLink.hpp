0f483bdaf77ff819e8f1d154e40a42a9e7818166
fallenoak <git@fallenoak.me>
2023-01-08 01:13:43 -0600

    feat(net): handle incoming data in Grunt::ClientLink

--- src/net/grunt/ClientLink.hpp
+++ src/net/grunt/ClientLink.hpp
@@ -7,14 +7,24 @@
 #include "net/grunt/Timer.hpp"
 #include "net/srp/SRP6_Client.hpp"
 #include "net/Types.hpp"
+#include <common/DataStore.hpp>
 #include <storm/Thread.hpp>
 
-class CDataStore;
 class WowConnection;
 
 class Grunt::ClientLink : public WowConnectionResponse, Grunt::Pending, Grunt::Timer::Event {
     public:
         // Types
+        enum COMMAND {
+            CMD_AUTH_LOGON_CHALLENGE        = 0,
+            CMD_AUTH_LOGON_PROOF            = 1,
+            CMD_AUTH_RECONNECT_CHALLENGE    = 2,
+            CMD_AUTH_RECONNECT_PROOF        = 3,
+            CMD_REALM_LIST                  = 16,
+            CMD_XFER_INITIATE               = 48,
+            CMD_XFER_DATA                   = 49,
+        };
+
         struct Logon {
             const char* accountName;
             const char* password;
@@ -34,6 +44,7 @@ class Grunt::ClientLink : public WowConnectionResponse, Grunt::Pending, Grunt::T
         int32_t m_state;
         SRP6_Client m_srpClient;
         SCritSect m_critSect;
+        CDataStore m_datastore1B0;
         WowConnection* m_connection = nullptr;
         ClientResponse* m_clientResponse;
         char m_accountName[1280];
@@ -41,10 +52,18 @@ class Grunt::ClientLink : public WowConnectionResponse, Grunt::Pending, Grunt::T
         // Virtual member functions
         virtual void WCConnected(WowConnection* conn, WowConnection* inbound, uint32_t timeStamp, const NETCONNADDR* addr);
         virtual void WCCantConnect(WowConnection* conn, uint32_t timeStamp, NETCONNADDR* addr);
+        virtual void WCDataReady(WowConnection* conn, uint32_t timeStamp, uint8_t* data, int32_t len);
         virtual void Call();
 
         // Member functions
         ClientLink(Grunt::ClientResponse& clientResponse);
+        int32_t CmdAuthLogonChallenge(CDataStore& msg);
+        int32_t CmdAuthLogonProof(CDataStore& msg);
+        int32_t CmdAuthReconnectChallenge(CDataStore& msg);
+        int32_t CmdAuthReconnectProof(CDataStore& msg);
+        int32_t CmdRealmList(CDataStore& msg);
+        int32_t CmdXferData(CDataStore& msg);
+        int32_t CmdXferInitiate(CDataStore& msg);
         void Connect(const char* a2);
         void Disconnect();
         void LogonNewSession(const Logon& logon);
