8745a5102d06eda91583e676aabed6dfa3a28bbf
fallenoak <git@fallenoak.me>
2023-02-14 22:33:39 -0600

    feat(db): load realm categories db

--- src/db/Db.cpp
+++ src/db/Db.cpp
@@ -2,6 +2,7 @@
 #include "db/WowClientDB_Base.hpp"
 
 WowClientDB<AchievementRec> g_achievementDB;
+WowClientDB<Cfg_CategoriesRec> g_cfg_CategoriesDB;
 
 void LoadDB(WowClientDB_Base* db, const char* filename, int32_t linenumber) {
     db->Load(filename, linenumber);
@@ -9,6 +10,7 @@ void LoadDB(WowClientDB_Base* db, const char* filename, int32_t linenumber) {
 
 void StaticDBLoadAll(void (*loadFn)(WowClientDB_Base*, const char*, int32_t)) {
     loadFn(&g_achievementDB, __FILE__, __LINE__);
+    loadFn(&g_cfg_CategoriesDB, __FILE__, __LINE__);
 };
 
 void ClientDBInitialize() {
---
+++ src/db/rec/Cfg_CategoriesRec.cpp
@@ -0,0 +1,45 @@
+#include "db/rec/Cfg_CategoriesRec.hpp"
+#include "util/SFile.hpp"
+
+const char* Cfg_CategoriesRec::GetFilename() {
+    return "DBFilesClient\\Cfg_Categories.dbc";
+}
+
+bool Cfg_CategoriesRec::Read(SFile* f, const char* stringBuffer) {
+    uint32_t nameOfs[16];
+    uint32_t nameMask;
+
+    if (
+        !SFile::Read(f, &this->m_ID, sizeof(this->m_ID), nullptr, nullptr, nullptr)
+        || !SFile::Read(f, &this->m_localeMask, sizeof(this->m_localeMask), nullptr, nullptr, nullptr)
+        || !SFile::Read(f, &this->m_createCharsetMask, sizeof(this->m_createCharsetMask), nullptr, nullptr, nullptr)
+        || !SFile::Read(f, &this->m_flags, sizeof(this->m_flags), nullptr, nullptr, nullptr)
+        || !SFile::Read(f, &nameOfs[0], sizeof(nameOfs[0]), nullptr, nullptr, nullptr)
+        || !SFile::Read(f, &nameOfs[1], sizeof(nameOfs[1]), nullptr, nullptr, nullptr)
+        || !SFile::Read(f, &nameOfs[2], sizeof(nameOfs[2]), nullptr, nullptr, nullptr)
+        || !SFile::Read(f, &nameOfs[3], sizeof(nameOfs[3]), nullptr, nullptr, nullptr)
+        || !SFile::Read(f, &nameOfs[4], sizeof(nameOfs[4]), nullptr, nullptr, nullptr)
+        || !SFile::Read(f, &nameOfs[5], sizeof(nameOfs[5]), nullptr, nullptr, nullptr)
+        || !SFile::Read(f, &nameOfs[6], sizeof(nameOfs[6]), nullptr, nullptr, nullptr)
+        || !SFile::Read(f, &nameOfs[7], sizeof(nameOfs[7]), nullptr, nullptr, nullptr)
+        || !SFile::Read(f, &nameOfs[8], sizeof(nameOfs[8]), nullptr, nullptr, nullptr)
+        || !SFile::Read(f, &nameOfs[9], sizeof(nameOfs[9]), nullptr, nullptr, nullptr)
+        || !SFile::Read(f, &nameOfs[10], sizeof(nameOfs[10]), nullptr, nullptr, nullptr)
+        || !SFile::Read(f, &nameOfs[11], sizeof(nameOfs[11]), nullptr, nullptr, nullptr)
+        || !SFile::Read(f, &nameOfs[12], sizeof(nameOfs[12]), nullptr, nullptr, nullptr)
+        || !SFile::Read(f, &nameOfs[13], sizeof(nameOfs[13]), nullptr, nullptr, nullptr)
+        || !SFile::Read(f, &nameOfs[14], sizeof(nameOfs[14]), nullptr, nullptr, nullptr)
+        || !SFile::Read(f, &nameOfs[15], sizeof(nameOfs[15]), nullptr, nullptr, nullptr)
+        || !SFile::Read(f, &nameMask, sizeof(nameMask), nullptr, nullptr, nullptr)
+    ) {
+        return false;
+    }
+
+    if (stringBuffer) {
+        this->m_name = &stringBuffer[nameOfs[0]];
+    } else {
+        this->m_name = "";
+    }
+
+    return true;
+}
