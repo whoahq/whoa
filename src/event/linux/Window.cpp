2bd6d063b53b10a0b3bb28429a3ddcf406007636
fallenoak <git@fallenoak.me>
2023-03-14 17:00:31 -0500

    feat(event): pass custom window proc in gx device creation

---
+++ src/event/linux/Window.cpp
@@ -0,0 +1,5 @@
+#include "event/Window.hpp"
+
+int32_t OsWindowProc(void* window, uint32_t message, uint32_t wparam, int32_t lparam) {
+    return 0;
+}
---
+++ src/event/win/Window.cpp
@@ -0,0 +1,7 @@
+#include "event/Window.hpp"
+#include <windows.h>
+
+int32_t OsWindowProc(void* window, uint32_t message, uint32_t wparam, int32_t lparam) {
+    // TODO
+    return 0;
+}
