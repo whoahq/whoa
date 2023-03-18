b8ebabaea59756516b8ff19ee9794a7692e4ad65
fallenoak <git@fallenoak.me>
2023-03-17 23:41:25 -0500

    fix(event): add missing break

--- src/event/win/Input.cpp
+++ src/event/win/Input.cpp
@@ -272,6 +272,8 @@ int32_t OsWindowProc(void* window, uint32_t message, uintptr_t wparam, intptr_t 
             OsQueuePut(OS_INPUT_MOUSE_MOVE, 0, mousePos.x, mousePos.y, 0);
             SaveMouse(mousePos, hwnd);
         }
+
+        break;
     }
 
     default:
