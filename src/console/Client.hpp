219fb1aad601a16374cf85b1e15e092ba30212e4
Tristan Cormier <cormiert2@outlook.com>
2023-04-01 15:08:04 -0400

    feat(console): add HIGHLIGHTSTATE enum

--- src/console/Client.hpp
+++ src/console/Client.hpp
@@ -3,6 +3,14 @@
 
 #include "gx/CGxFormat.hpp"
 
+enum HIGHLIGHTSTATE
+{
+    HS_NONE = 0x0,
+    HS_HIGHLIGHTING = 0x1,
+    HS_ENDHIGHLIGHT = 0x2,
+    NUM_HIGHLIGHTSTATES
+};
+
 struct DefaultSettings {
     CGxFormat format;
 };
