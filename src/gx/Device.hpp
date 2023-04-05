0aa9324f6be1b2046d1785044389c76ee7b1c54c
Tristan Cormier <cormiert2@outlook.com>
2023-04-04 23:29:31 -0400

    feat(gx): add GxDevExists()

--- src/gx/Device.hpp
+++ src/gx/Device.hpp
@@ -11,6 +11,8 @@ extern CGxDevice* g_theGxDevicePtr;
 
 CGxDevice* GxDevCreate(EGxApi api, int32_t (*windowProc)(void* window, uint32_t message, uintptr_t wparam, intptr_t lparam), const CGxFormat& format);
 
+bool GxDevExists();
+
 EGxApi GxDevApi(void);
 
 void* GxDevWindow();
