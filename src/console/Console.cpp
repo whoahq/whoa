12a1f33093074af28abc5177fde6e3aba87ce1fd
Tristan Cormier <cormiert2@outlook.com>
2023-04-08 11:31:07 -0400

    feat(console): add ConsoleGetHotKey() and ConsoleSetHotKey()

--- src/console/Console.cpp
+++ src/console/Console.cpp
@@ -2,6 +2,7 @@
 
 static int32_t s_active;
 static int32_t s_consoleAccessEnabled;
+static KEY s_consoleKey = KEY_TILDE;
 
 int32_t ConsoleAccessGetEnabled() {
     return s_consoleAccessEnabled;
@@ -15,6 +16,14 @@ int32_t ConsoleGetActive() {
     return s_active;
 }
 
+KEY ConsoleGetHotKey() {
+    return s_consoleKey;
+}
+
 void ConsoleSetActive(int32_t active) {
     s_active = active;
+}
+
+void ConsoleSetHotKey(KEY hotkey) {
+    s_consoleKey = hotkey;
 }
