3549abd87fa96142243404f26e7924b70c893afd
Tristan 'Natrist' Cormier <natrist@users.noreply.github.com>
2023-04-15 17:33:17 -0400

    feat(console): add helpers for console hotkey

--- src/console/Console.hpp
+++ src/console/Console.hpp
@@ -1,6 +1,7 @@
 #ifndef CONSOLE_CONSOLE_HPP
 #define CONSOLE_CONSOLE_HPP
 
+#include "event/Types.hpp"
 #include <cstdint>
 
 int32_t ConsoleAccessGetEnabled();
@@ -9,6 +10,10 @@ void ConsoleAccessSetEnabled(int32_t enable);
 
 int32_t ConsoleGetActive();
 
+KEY ConsoleGetHotKey();
+
 void ConsoleSetActive(int32_t active);
+
+void ConsoleSetHotKey(KEY hotkey);
 
 #endif  // ifndef CONSOLE_CONSOLE_HPP
