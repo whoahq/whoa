742e53f148a789b6a75c138915853c6377438ee9
superp00t <superp00t@tutanota.com>
2023-12-13 20:28:12 -0500

    feat(event): make OsInputGet dispatch queued events on Linux

--- src/event/linux/Input.cpp
+++ src/event/linux/Input.cpp
@@ -1,8 +1,14 @@
 #include "event/Input.hpp"
+#include <common/Time.hpp>
 
 int32_t OsInputGet(OSINPUT* id, int32_t* param0, int32_t* param1, int32_t* param2, int32_t* param3) {
-    // TODO
-    return 0;
+    if (Input::s_queueTail == Input::s_queueHead) {
+        return 0;
+    }
+
+    OsQueueSetParam(3, OsGetAsyncTimeMs());
+
+    return OsQueueGet(id, param0, param1, param2, param3);
 }
 
 void OsInputSetMouseMode(OS_MOUSE_MODE mode) {
