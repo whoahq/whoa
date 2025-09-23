94ceaf0d9694a13c6fe54cfb7bfb4a871e3af201
fallenoak <git@fallenoak.me>
2025-09-23 10:28:47 -0700

    feat(console): implement ConsoleInitializeClientCommand

---
+++ src/console/CommandHandlers.cpp
@@ -0,0 +1,24 @@
+#include "console/CommandHandlers.hpp"
+
+int32_t ConsoleCommand_Help(const char* command, const char* arguments) {
+    // TODO
+
+    return 0;
+}
+
+int32_t ConsoleCommand_Quit(const char* command, const char* arguments) {
+    // TODO
+    // ConsolePostClose()
+
+    return 0;
+}
+
+int32_t ConsoleCommand_SetMap(const char* command, const char* arguments) {
+    return 1;
+}
+
+int32_t ConsoleCommand_Ver(const char* command, const char* arguments) {
+    // TODO
+
+    return 0;
+}
