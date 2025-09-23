ae536ce528dda396de5e80e7636791a108a13068
fallenoak <git@fallenoak.me>
2025-09-23 10:06:15 -0700

    feat(console): implement ConsoleInitializeClientCVar

--- src/console/Client.cpp
+++ src/console/Client.cpp
@@ -1,9 +1,17 @@
 #include "console/Client.hpp"
+#include "CVar.hpp"
+
+static const char* s_filename;
 
 void ConsoleInitializeClientCommand() {
     // TODO
 }
 
-void ConsoleInitializeClientCVar(const char* a1) {
+void ConsoleInitializeClientCVar(const char* filename) {
+    s_filename = filename;
+
+    CVar::Initialize();
+
     // TODO
+    // CVar::Load(s_filename);
 }
