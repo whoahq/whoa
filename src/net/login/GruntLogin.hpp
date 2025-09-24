1d1398ed4f1fde9a5259818aa82f1451cf47d4b6
fallenoak <git@fallenoak.me>
2025-09-24 15:02:26 -0700

    feat(net): clean up and implement more of CmdAuthLogonChallenge

--- src/net/login/GruntLogin.hpp
+++ src/net/login/GruntLogin.hpp
@@ -16,9 +16,9 @@ class GruntLogin : public Login {
         virtual bool Connected(const NETADDR& addr);
         virtual void GetLogonMethod();
         virtual void GetVersionProof(const uint8_t* versionChallenge);
-        virtual void SetPinInfo(bool enabled, uint32_t a3, const uint8_t* a4);
-        virtual void SetMatrixInfo(bool enabled, uint8_t a3, uint8_t a4, uint8_t a5, uint8_t a6, bool a7, uint8_t a8, uint64_t a9, const uint8_t* a10, uint32_t a11);
-        virtual void SetTokenInfo(bool enabled, uint8_t tokenRequired);
+        virtual void SetPinInfo(bool enabled, uint32_t gridSeed, const uint8_t* salt);
+        virtual void SetMatrixInfo(bool enabled, uint8_t width, uint8_t height, uint8_t a5, uint8_t a6, bool a7, uint8_t challengeCount, uint64_t seed, const uint8_t* sessionKey, uint32_t a11);
+        virtual void SetTokenInfo(bool enabled, uint8_t required);
         virtual void LogonResult(Grunt::Result result, const uint8_t* sessionKey, uint32_t sessionKeyLen, uint16_t flags);
         virtual LOGIN_STATE NextSecurityState(LOGIN_STATE state);
         virtual int32_t GetServerId();
