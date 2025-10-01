9ed50a0eed9bba9f39a1f272e498ef9949c5bccf
fallenoak <git@fallenoak.me>
2025-10-01 16:47:57 -0500

    chore(console): move CATEGORY enum to types header

--- src/console/Command.hpp
+++ src/console/Command.hpp
@@ -1,6 +1,7 @@
 #ifndef CONSOLE_COMMAND_HPP
 #define CONSOLE_COMMAND_HPP
 
+#include "console/Types.hpp"
 #include <storm/Hash.hpp>
 #include <cstdint>
 
@@ -9,20 +10,6 @@
 #define MAX_CMD_LENGTH 64
 #define HISTORY_DEPTH 32
 #define NOHELP nullptr
-
-enum CATEGORY {
-    DEBUG,
-    GRAPHICS,
-    CONSOLE,
-    COMBAT,
-    GAME,
-    DEFAULT,
-    NET,
-    SOUND,
-    GM,
-    NONE,
-    LAST
-};
 
 struct CONSOLECOMMAND : TSHashObject<CONSOLECOMMAND, HASHKEY_STRI> {
     const char* command;
