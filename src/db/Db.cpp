087931e9b44d2073eba6a8b391b967a21b5a1b8e
fallenoak <git@fallenoak.me>
2025-10-05 22:03:48 -0500

    feat(db): add ChrClassesRec

--- src/db/Db.cpp
+++ src/db/Db.cpp
@@ -4,6 +4,7 @@
 WowClientDB<AchievementRec> g_achievementDB;
 WowClientDB<Cfg_CategoriesRec> g_cfg_CategoriesDB;
 WowClientDB<Cfg_ConfigsRec> g_cfg_ConfigsDB;
+WowClientDB<ChrClassesRec> g_chrClassesDB;
 WowClientDB<ChrRacesRec> g_chrRacesDB;
 WowClientDB<MapRec> g_mapDB;
 
@@ -15,6 +16,7 @@ void StaticDBLoadAll(void (*loadFn)(WowClientDB_Base*, const char*, int32_t)) {
     loadFn(&g_achievementDB, __FILE__, __LINE__);
     loadFn(&g_cfg_CategoriesDB, __FILE__, __LINE__);
     loadFn(&g_cfg_ConfigsDB, __FILE__, __LINE__);
+    loadFn(&g_chrClassesDB, __FILE__, __LINE__);
     loadFn(&g_chrRacesDB, __FILE__, __LINE__);
     loadFn(&g_mapDB, __FILE__, __LINE__);
 };
--- src/db/Db.hpp
+++ src/db/Db.hpp
@@ -5,12 +5,14 @@
 #include "db/rec/AchievementRec.hpp"
 #include "db/rec/Cfg_CategoriesRec.hpp"
 #include "db/rec/Cfg_ConfigsRec.hpp"
+#include "db/rec/ChrClassesRec.hpp"
 #include "db/rec/ChrRacesRec.hpp"
 #include "db/rec/MapRec.hpp"
 
 extern WowClientDB<AchievementRec> g_achievementDB;
 extern WowClientDB<Cfg_CategoriesRec> g_cfg_CategoriesDB;
 extern WowClientDB<Cfg_ConfigsRec> g_cfg_ConfigsDB;
+extern WowClientDB<ChrClassesRec> g_chrClassesDB;
 extern WowClientDB<ChrRacesRec> g_chrRacesDB;
 extern WowClientDB<MapRec> g_mapDB;
 
