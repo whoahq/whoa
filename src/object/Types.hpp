0e1a8582c1600864209c2f6dad0b635776fec0f6
fallenoak <git@fallenoak.me>
2025-10-05 20:41:51 -0500

    feat(object): add UNIT_SEX enum

---
+++ src/object/Types.hpp
@@ -0,0 +1,12 @@
+#ifndef OBJECT_TYPES_HPP
+#define OBJECT_TYPES_HPP
+
+enum UNIT_SEX {
+    UNITSEX_MALE    = 0,
+    UNITSEX_FEMALE  = 1,
+    UNITSEX_NONE    = 2,
+    UNITSEX_LAST,
+    UNITSEX_BOTH    = 3,
+};
+
+#endif
