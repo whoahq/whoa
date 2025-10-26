29bee10e11fd1200b16f0b25f5b692e2c768efae
fallenoak <git@fallenoak.me>
2025-10-25 22:43:51 -0500

    feat(db): add CharacterFacialHairStylesRec

--- src/db/Db.cpp
+++ src/db/Db.cpp
@@ -5,6 +5,7 @@ WowClientDB<AchievementRec> g_achievementDB;
 WowClientDB<AreaTableRec> g_areaTableDB;
 WowClientDB<Cfg_CategoriesRec> g_cfg_CategoriesDB;
 WowClientDB<Cfg_ConfigsRec> g_cfg_ConfigsDB;
+WowClientDB<CharacterFacialHairStylesRec> g_characterFacialHairStylesDB;
 WowClientDB<CharHairGeosetsRec> g_charHairGeosetsDB;
 WowClientDB<CharSectionsRec> g_charSectionsDB;
 WowClientDB<ChrClassesRec> g_chrClassesDB;
@@ -22,6 +23,7 @@ void StaticDBLoadAll(void (*loadFn)(WowClientDB_Base*, const char*, int32_t)) {
     loadFn(&g_areaTableDB, __FILE__, __LINE__);
     loadFn(&g_cfg_CategoriesDB, __FILE__, __LINE__);
     loadFn(&g_cfg_ConfigsDB, __FILE__, __LINE__);
+    loadFn(&g_characterFacialHairStylesDB, __FILE__, __LINE__);
     loadFn(&g_charHairGeosetsDB, __FILE__, __LINE__);
     loadFn(&g_charSectionsDB, __FILE__, __LINE__);
     loadFn(&g_chrClassesDB, __FILE__, __LINE__);
--- src/db/Db.hpp
+++ src/db/Db.hpp
@@ -6,6 +6,7 @@
 #include "db/rec/AreaTableRec.hpp"
 #include "db/rec/Cfg_CategoriesRec.hpp"
 #include "db/rec/Cfg_ConfigsRec.hpp"
+#include "db/rec/CharacterFacialHairStylesRec.hpp"
 #include "db/rec/CharHairGeosetsRec.hpp"
 #include "db/rec/CharSectionsRec.hpp"
 #include "db/rec/ChrClassesRec.hpp"
@@ -18,6 +19,7 @@ extern WowClientDB<AchievementRec> g_achievementDB;
 extern WowClientDB<AreaTableRec> g_areaTableDB;
 extern WowClientDB<Cfg_CategoriesRec> g_cfg_CategoriesDB;
 extern WowClientDB<Cfg_ConfigsRec> g_cfg_ConfigsDB;
+extern WowClientDB<CharacterFacialHairStylesRec> g_characterFacialHairStylesDB;
 extern WowClientDB<CharHairGeosetsRec> g_charHairGeosetsDB;
 extern WowClientDB<CharSectionsRec> g_charSectionsDB;
 extern WowClientDB<ChrClassesRec> g_chrClassesDB;
