d6cf3063303a9e171a20b46f10a7185af2228ccd
fallenoak <git@fallenoak.me>
2025-10-27 22:24:23 -0500

    fix(gx): members of C4Pixel should be unsigned

--- src/gx/Types.hpp
+++ src/gx/Types.hpp
@@ -338,10 +338,10 @@ enum PIXEL_FORMAT {
 };
 
 struct C4Pixel {
-    char b;
-    char g;
-    char r;
-    char a;
+    uint8_t b;
+    uint8_t g;
+    uint8_t r;
+    uint8_t a;
 };
 
 struct MipBits {
