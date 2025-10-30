9b46d51508beb644fc9b34bf511aa5135770958a
Tristan 'Natrist' Cormier <natrist@users.noreply.github.com>
2025-10-29 20:46:44 -0400

    feat(console): add CONSOLELINE class (#120)

--- src/console/Screen.hpp
+++ src/console/Screen.hpp
@@ -1,6 +1,23 @@
 #ifndef CONSOLE_SCREEN_HPP
 #define CONSOLE_SCREEN_HPP
 
+#include "console/Types.hpp"
+#include <storm/List.hpp>
+
+class CGxString;
+
+class CONSOLELINE : public TSLinkedNode<CONSOLELINE> {
+    public:
+        // Member variables
+        char* buffer;
+        uint32_t chars;
+        uint32_t charsalloc;
+        uint32_t inputpos;
+        uint32_t inputstart;
+        COLOR_T colorType;
+        CGxString* fontPointer;
+};
+
 void ConsoleScreenAnimate(float elapsedSec);
 
 void ConsoleScreenInitialize(const char* title);
