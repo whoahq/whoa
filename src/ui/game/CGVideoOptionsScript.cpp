6ec4d2eae681901257a796628a21ba93185e38b5
fallenoak <git@fallenoak.me>
2025-11-09 17:20:40 -0600

    feat(ui): implement Script_GetScreenResolutions

--- src/ui/game/CGVideoOptionsScript.cpp
+++ src/ui/game/CGVideoOptionsScript.cpp
@@ -1,12 +1,43 @@
 #include "ui/game/CGVideoOptionsScript.hpp"
-#include "ui/game/CGVideoOptions.hpp"
+#include "console/CVar.hpp"
+#include "console/Detect.hpp"
 #include "ui/Types.hpp"
+#include "ui/game/CGVideoOptions.hpp"
 #include "util/Lua.hpp"
 #include "util/Unimplemented.hpp"
+#include <tempest/Vector.hpp>
+
+static TSGrowableArray<C2iVector> s_screenResolutions;
+
+void SetupResolutions() {
+    if (s_screenResolutions.Count()) {
+        return;
+    }
+
+    auto widescreenCvar = CVar::Lookup("widescreen");
+    int32_t widescreen = widescreenCvar ? widescreenCvar->GetInt() : 0;
+
+    ConsoleDetectGetResolutions(s_screenResolutions, widescreen);
+}
 
 int32_t Script_GetScreenResolutions(lua_State* L) {
-    WHOA_UNIMPLEMENTED(0);
+    SetupResolutions();
+
+    lua_checkstack(L, s_screenResolutions.Count());
+
+    char resolutionStr[64];
+
+    for (uint32_t i = 0; i < s_screenResolutions.Count(); i++) {
+        auto& resolution = s_screenResolutions[i];
+
+        SStrPrintf(resolutionStr, sizeof(resolutionStr), "%dx%d", resolution.x, resolution.y);
+
+        lua_pushstring(L, resolutionStr);
+    }
+
+    return s_screenResolutions.Count();
 }
+
 int32_t Script_GetCurrentResolution(lua_State* L) {
     WHOA_UNIMPLEMENTED(0);
 }
