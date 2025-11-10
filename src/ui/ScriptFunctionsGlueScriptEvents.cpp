ee0dbb3f9299e505f335ddd434d65d314fca8e0c
fallenoak <git@fallenoak.me>
2025-11-09 21:04:05 -0600

    feat(ui): implement Script_GetBuildInfo

--- src/ui/ScriptFunctionsGlueScriptEvents.cpp
+++ src/ui/ScriptFunctionsGlueScriptEvents.cpp
@@ -18,7 +18,19 @@ int32_t Script_IsShiftKeyDown(lua_State* L) {
 }
 
 int32_t Script_GetBuildInfo(lua_State* L) {
-    WHOA_UNIMPLEMENTED(0);
+    // TODO use compile-time defines to control these values
+
+    auto versionText = FrameScript_GetText("VERSION", -1, GENDER_NOT_APPLICABLE);
+    lua_pushstring(L, versionText);
+
+    auto releaseText = FrameScript_GetText("RELEASE_BUILD", -1, GENDER_NOT_APPLICABLE);
+    lua_pushstring(L, releaseText);
+
+    lua_pushstring(L, "3.3.5");
+    lua_pushstring(L, "12340");
+    lua_pushstring(L, "Jun 24 2010");
+
+    return 5;
 }
 
 int32_t Script_GetLocale(lua_State* L) {
