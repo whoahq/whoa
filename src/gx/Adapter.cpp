da7e27b79a226d02fce7f6fa36c26e31bad0e3f5
fallenoak <git@fallenoak.me>
2025-11-16 22:03:57 -0600

    feat(gx): add GxAdapterFormats

--- src/gx/Adapter.cpp
+++ src/gx/Adapter.cpp
@@ -1,5 +1,10 @@
 #include "gx/Adapter.hpp"
 #include "gx/CGxDevice.hpp"
+#include "gx/CGxMonitorMode.hpp"
+
+int32_t GxAdapterFormats(EGxApi api, TSGrowableArray<CGxFormat>& adapterFormats) {
+    return CGxDevice::AdapterFormats(api, adapterFormats);
+}
 
 int32_t GxAdapterMonitorModes(TSGrowableArray<CGxMonitorMode>& monitorModes) {
     return CGxDevice::AdapterMonitorModes(monitorModes);
