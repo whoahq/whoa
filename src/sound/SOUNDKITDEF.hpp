d4817d8844c2eb981e201e0adca947a842f860fa
fallenoak <git@fallenoak.me>
2025-11-22 10:36:14 -0600

    feat(sound): add SOUNDKITDEF

---
+++ src/sound/SOUNDKITDEF.hpp
@@ -0,0 +1,27 @@
+#ifndef SOUND_SOUND_KIT_DEF_HPP
+#define SOUND_SOUND_KIT_DEF_HPP
+
+#include "db/Db.hpp"
+#include <fmod.hpp>
+#include <cstdint>
+
+class SOUNDKITDEF {
+    public:
+        const char* name;
+        const char* files[10] = {};
+        float minDistance;
+        float distanceCutoff;
+        float volume;
+        int32_t ID;
+        uint32_t flags;
+        int32_t fileCount = 0;
+        int32_t selectedIndex = -1;
+        // TODO: 0x48 - 0x94
+        FMOD::SoundGroup* soundGroup1;
+        FMOD::SoundGroup* soundGroup2;
+        // TODO: 0xA0 - 0xA4
+        int32_t advancedID = 0;
+        SoundEntriesAdvancedRec* advanced;
+};
+
+#endif
