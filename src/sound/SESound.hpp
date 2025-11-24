74289ed6b276017496bd334ae985bc84bd0cd1a4
fallenoak <git@fallenoak.me>
2025-11-23 20:02:23 -0600

    feat(sound): add SESoundInternal

--- src/sound/SESound.hpp
+++ src/sound/SESound.hpp
@@ -1,6 +1,7 @@
 #ifndef SOUND_SE_SOUND_HPP
 #define SOUND_SE_SOUND_HPP
 
+#include "sound/SESoundInternal.hpp"
 #include <fmod.hpp>
 #include <cstdint>
 
@@ -14,6 +15,12 @@ class SESound {
         static FMOD::SoundGroup* CreateSoundGroup(const char* name, int32_t maxAudible);
         static void Init(int32_t maxChannels, int32_t (*a2), int32_t enableReverb, int32_t enableSoftwareHRTF, int32_t* numChannels, int32_t* outputDriverIndex, const char* outputDriverName, void (*a8), int32_t a9);
         static void Log_Write(int32_t line, const char* file, FMOD_RESULT result, const char* fmt, ...);
+
+        // Member variables
+        SESoundInternal* m_internal = nullptr;
+
+        // Member functions
+        // TODO
 };
 
 #endif
---
+++ src/sound/SESoundInternal.hpp
@@ -0,0 +1,24 @@
+#ifndef SOUND_SE_SOUND_INTERNAL_HPP
+#define SOUND_SE_SOUND_INTERNAL_HPP
+
+class SESoundInternal {
+    public:
+        // TODO
+};
+
+class SEDiskSound : public SESoundInternal {
+    public:
+        // TODO
+};
+
+class SEMemorySound : public SESoundInternal {
+    public:
+        // TODO
+};
+
+class SEStreamedSound : public SESoundInternal {
+    public:
+        // TODO
+};
+
+#endif
