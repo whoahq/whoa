73001e7e457443f604f50dfd42c04a2735786a29
Tristan Cormier <cormiert2@outlook.com>
2025-11-27 19:23:19 -0500

    feat(ui): implement CSimpleEditBox_SetText

--- src/ui/CSimpleEditBoxScript.cpp
+++ src/ui/CSimpleEditBoxScript.cpp
@@ -129,7 +129,18 @@ int32_t CSimpleEditBox_Insert(lua_State* L) {
 }
 
 int32_t CSimpleEditBox_SetText(lua_State* L) {
-    WHOA_UNIMPLEMENTED(0);
+    auto type = CSimpleEditBox::GetObjectType();
+    auto editBox = static_cast<CSimpleEditBox*>(FrameScript_GetObjectThis(L, type));
+
+    if (!lua_isstring(L, 2)) {
+        return luaL_error(L, "Usage: %s:SetText(\"text\")", editBox->GetDisplayName());
+    }
+
+    // TODO lua_tainted
+    auto text = lua_tostring(L, 2);
+    editBox->SetText(text, nullptr);
+
+    return 0;
 }
 
 int32_t CSimpleEditBox_GetText(lua_State* L) {
