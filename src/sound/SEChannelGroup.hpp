25d2d36636adbc05248eefc9aa838d0849841d02
fallenoak <git@fallenoak.me>
2025-11-28 22:51:12 -0600

    feat(sound): add SEChannelGroup

---
+++ src/sound/SEChannelGroup.hpp
@@ -0,0 +1,16 @@
+#ifndef SOUND_SE_CHANNEL_GROUP_HPP
+#define SOUND_SE_CHANNEL_GROUP_HPP
+
+#include <cstdint>
+
+class SEChannelGroup {
+    public:
+        // Member variables
+        uint32_t m_nameHash;
+        int32_t m_parentChannelGroup;
+        float m_volume;
+        float m_muteVolume;
+        bool m_needsUpdate;
+};
+
+#endif
