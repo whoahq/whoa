41641778d9cdf02c64674c0f474d30639e4a2f72
fallenoak <git@fallenoak.me>
2025-12-15 19:48:41 -0600

    chore(test): add GxuFontCreateFont tests

--- test/Test.cpp
+++ test/Test.cpp
@@ -1,2 +1,15 @@
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
+#include "gx/font/FreeType.hpp"
+
+int main(int argc, char* argv[]) {
+    // Global initialization
+    FreeTypeInitialize();
+
+    int result = Catch::Session().run( argc, argv );
+
+    // Global cleanup
+    // TODO
+
+    return result;
+}
