0e685d6fbdfe30ea6f11d4e2d2d8428ae4d55b1f
fallenoak <git@fallenoak.me>
2026-01-05 22:31:29 -0600

    feat(object): add ClntObjMgrPush

--- src/object/client/ObjMgr.cpp
+++ src/object/client/ObjMgr.cpp
@@ -1,5 +1,4 @@
 #include "object/client/ObjMgr.hpp"
-#include "object/client/ClntObjMgr.hpp"
 
 #if defined(WHOA_SYSTEM_WIN)
 static thread_local ClntObjMgr* s_curMgr;
@@ -7,10 +6,21 @@ static thread_local ClntObjMgr* s_curMgr;
 static ClntObjMgr* s_curMgr;
 #endif
 
+static ClntObjMgr* s_savMgr;
+
 uint32_t ClntObjMgrGetMapID() {
     if (!s_curMgr) {
         return 0;
     }
 
     return s_curMgr->m_mapID;
+}
+
+void ClntObjMgrPush(ClntObjMgr* mgr) {
+    if (s_savMgr || mgr == s_curMgr) {
+        return;
+    }
+
+    s_savMgr = s_curMgr;
+    s_curMgr = mgr;
 }
--- src/object/client/ObjMgr.hpp
+++ src/object/client/ObjMgr.hpp
@@ -1,8 +1,11 @@
 #ifndef OBJECT_CLIENT_OBJ_MGR_HPP
 #define OBJECT_CLIENT_OBJ_MGR_HPP
 
+#include "object/client/ClntObjMgr.hpp"
 #include <cstdint>
 
 uint32_t ClntObjMgrGetMapID();
+
+void ClntObjMgrPush(ClntObjMgr* mgr);
 
 #endif
