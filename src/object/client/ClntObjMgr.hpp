35787c4ac8c0708cd632408529a4fc9d99627029
fallenoak <git@fallenoak.me>
2026-01-05 22:27:41 -0600

    feat(object): implement ClntObjMgrGetMapID

---
+++ src/object/client/ClntObjMgr.hpp
@@ -0,0 +1,16 @@
+#ifndef OBJECT_CLIENT_CLNT_OBJ_MGR_HPP
+#define OBJECT_CLIENT_CLNT_OBJ_MGR_HPP
+
+#include <cstdint>
+
+class ClientConnection;
+
+class ClntObjMgr {
+    public:
+        // Member variables
+        uint64_t m_activePlayer = 0;
+        uint32_t m_mapID = 0;
+        ClientConnection* m_net = nullptr;
+};
+
+#endif
--- src/object/client/ObjMgr.cpp
+++ src/object/client/ObjMgr.cpp
@@ -1,6 +1,16 @@
 #include "object/client/ObjMgr.hpp"
+#include "object/client/ClntObjMgr.hpp"
+
+#if defined(WHOA_SYSTEM_WIN)
+static thread_local ClntObjMgr* s_curMgr;
+#else
+static ClntObjMgr* s_curMgr;
+#endif
 
 uint32_t ClntObjMgrGetMapID() {
-    // TODO
-    return -1;
+    if (!s_curMgr) {
+        return 0;
+    }
+
+    return s_curMgr->m_mapID;
 }
