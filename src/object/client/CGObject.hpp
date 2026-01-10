f6236a3ebf0c7c7738b735094308410ffb35d428
fallenoak <git@fallenoak.me>
2026-01-09 20:47:29 -0600

    feat(object): add members to CGObject

--- src/object/client/CGObject.hpp
+++ src/object/client/CGObject.hpp
@@ -1,6 +1,7 @@
 #ifndef OBJECT_CLIENT_CG_OBJECT_HPP
 #define OBJECT_CLIENT_CG_OBJECT_HPP
 
+#include "object/Types.hpp"
 #include <cstdint>
 
 struct CGObjectData {
@@ -11,6 +12,12 @@ class CGObject {
     public:
         // Public static functions
         static uint32_t TotalFieldsSaved();
+
+        // Public member variables
+        CGObjectData* m_obj;
+        uint32_t* m_objSaved;
+        uint32_t m_memHandle;
+        OBJECT_TYPE_ID m_typeID;
 };
 
 #endif
