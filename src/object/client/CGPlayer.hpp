2695b584fd748c946f624e52ab3fca2ed34378a2
fallenoak <git@fallenoak.me>
2026-01-11 13:15:18 -0600

    feat(object): add partial layout for CGPlayerData

--- src/object/client/CGPlayer.hpp
+++ src/object/client/CGPlayer.hpp
@@ -1,9 +1,52 @@
 #ifndef OBJECT_CLIENT_CG_PLAYER_HPP
 #define OBJECT_CLIENT_CG_PLAYER_HPP
 
+#include "object/Types.hpp"
 #include <cstdint>
 
+struct CQuestLogData {
+    int32_t questID;
+    uint32_t field2;
+    uint32_t field3;
+    uint32_t field4;
+    uint32_t field5;
+};
+
+// TODO is this VisibleItem_C?
+struct CVisibleItemData {
+    int32_t entryID;
+    uint32_t enchantment;
+};
+
 struct CGPlayerData {
+    uint64_t duelArbiter;
+    uint32_t flags;
+    uint32_t guildID;
+    uint32_t guildRank;
+    int32_t pad1;
+    int32_t pad2;
+    int32_t pad3;
+    uint32_t duelTeam;
+    int32_t guildTimestamp;
+    CQuestLogData questLog[25];
+    CVisibleItemData visibleItems[19];
+    int32_t chosenTitle;
+    int32_t fakeInebriation;
+    int32_t pad4;
+    uint64_t invSlots[NUM_INVENTORY_SLOTS];
+    uint64_t packSlots[16];
+    uint64_t bankSlots[28];
+    uint64_t bankBagSlots[7];
+    uint64_t vendorBuybackSlots[12];
+    uint64_t keyringSlots[32];
+    uint64_t currencyTokenSlots[32];
+    uint64_t farsightObject;
+    uint64_t knownTitles;
+    uint64_t knownTitles2;
+    uint64_t knownTitles3;
+    uint64_t knownCurrencies;
+    uint32_t xp;
+    uint32_t nextLevelXP;
     // TODO
 };
 
