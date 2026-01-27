ac0930497c4e2db8bc7d3fe0dad640cf13e0020e
fallenoak <git@fallenoak.me>
2026-01-27 17:54:56 -0600

    feat(ui): add CGTooltip::GetObjectType

--- src/ui/game/CGTooltip.cpp
+++ src/ui/game/CGTooltip.cpp
@@ -1,9 +1,19 @@
 #include "ui/game/CGTooltip.hpp"
 
+int32_t CGTooltip::s_objectType;
+
 CSimpleFrame* CGTooltip::Create(CSimpleFrame* parent) {
     // TODO use CDataAllocator
 
     return STORM_NEW(CGTooltip)(parent);
+}
+
+int32_t CGTooltip::GetObjectType() {
+    if (!CGTooltip::s_objectType) {
+        CGTooltip::s_objectType = ++FrameScript_Object::s_objectTypes;
+    }
+
+    return CGTooltip::s_objectType;
 }
 
 CGTooltip::CGTooltip(CSimpleFrame* parent) : CSimpleFrame(parent) {
--- src/ui/game/CGTooltip.hpp
+++ src/ui/game/CGTooltip.hpp
@@ -5,8 +5,12 @@
 
 class CGTooltip : public CSimpleFrame {
     public:
+        // Static variables
+        static int32_t s_objectType;
+
         // Static functions
         static CSimpleFrame* Create(CSimpleFrame* parent);
+        static int32_t GetObjectType();
 
         // Member variables
         // TODO
