e5cc9de4866e6d96a8395a8a0d3d5c9e7a6cd223
fallenoak <git@fallenoak.me>
2026-02-18 19:34:52 -0600

    feat(ui): add CGWorldFrame::OnFrameSizeChanged

--- src/ui/game/CGWorldFrame.cpp
+++ src/ui/game/CGWorldFrame.cpp
@@ -1,6 +1,8 @@
 #include "ui/game/CGWorldFrame.hpp"
+#include "gx/Coordinate.hpp"
 #include "gx/Shader.hpp"
 #include "gx/Transform.hpp"
+#include "ui/game/CGCamera.hpp"
 #include "ui/game/PlayerName.hpp"
 #include <storm/Memory.hpp>
 #include <tempest/Matrix.hpp>
@@ -46,6 +48,10 @@ CGWorldFrame::CGWorldFrame(CSimpleFrame* parent) : CSimpleFrame(parent) {
     this->EnableEvent(SIMPLE_EVENT_MOUSEWHEEL, -1);
 
     // TODO
+
+    this->m_camera = STORM_NEW(CGCamera);
+
+    // TODO
 }
 
 void CGWorldFrame::OnFrameRender(CRenderBatch* batch, uint32_t layer) {
@@ -54,6 +60,29 @@ void CGWorldFrame::OnFrameRender(CRenderBatch* batch, uint32_t layer) {
     if (layer == DRAWLAYER_BACKGROUND) {
         batch->QueueCallback(&CGWorldFrame::RenderWorld, this);
     }
+}
+
+void CGWorldFrame::OnFrameSizeChanged(const CRect& rect) {
+    this->CSimpleFrame::OnFrameSizeChanged(rect);
+
+    // Screen rect (DDC)
+    this->m_screenRect.minX = std::max(this->m_rect.minX, 0.0f);
+    this->m_screenRect.minY = std::max(this->m_rect.minY, 0.0f);
+    this->m_screenRect.maxX = std::min(this->m_rect.maxX, NDCToDDCWidth(1.0f));
+    this->m_screenRect.maxY = std::min(this->m_rect.maxY, NDCToDDCHeight(1.0f));
+
+    // Camera aspect ratio
+    if (this->m_camera) {
+        this->m_camera->SetScreenAspect(this->m_screenRect);
+    }
+
+    // Viewport (NDC)
+    DDCToNDC(this->m_rect.minX, this->m_rect.minY, &this->m_viewport.minX, &this->m_viewport.minY);
+    DDCToNDC(this->m_rect.maxX, this->m_rect.maxY, &this->m_viewport.maxX, &this->m_viewport.maxY);
+    this->m_viewport.minX = std::max(this->m_viewport.minX, 0.0f);
+    this->m_viewport.minY = std::max(this->m_viewport.minY, 0.0f);
+    this->m_viewport.maxX = std::min(this->m_viewport.maxX, 1.0f);
+    this->m_viewport.maxY = std::min(this->m_viewport.maxY, 1.0f);
 }
 
 void CGWorldFrame::OnWorldRender() {
